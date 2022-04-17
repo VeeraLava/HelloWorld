4a1c494a8d81e2b38187764166c8555d94f0a32c
sunil <sunilkumarbv1906@gmail.com>
2022-04-17 13:08:45 +0530

    Adding Array App

---
+++ AddArrays/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  AddArrays
+//
+//  Created by sunil on 19/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addArrays(arr1: [1,2,3], arr2: [4,5,6])
+        
+    }
+
+    func addArrays(arr1:[Int],arr2:[Int]){
+        
+        var finalArray:[Int] = []
+        
+        for i in 0...arr1.count - 1{
+            if arr1.count == arr2.count{
+                finalArray.insert(arr1[i] + arr2[i], at: i)
+            }
+        }
+        print(finalArray)
+    }
+
+    
+
+
+}
+
